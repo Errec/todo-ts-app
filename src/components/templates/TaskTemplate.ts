d6d0fdc7c1b410fcddfc2fa3ed66dda4b11fca4a
Raniro Coelho <ranirocoelho@gmail.com>
2024-06-11 21:39:45 -0300

    Add TaskTemplate component for creating a task template

---
+++ src/components/templates/TaskTemplate.ts
@@ -0,0 +1,47 @@
+import { Input } from '../atoms/Input';
+import { Button } from '../atoms/Button';
+import { TaskList } from '../organisms/TaskList';
+import { Task } from '../../types/Task';
+
+/**
+ * Creates a task template component.
+ * @param tasks - An array of tasks.
+ * @param onAddTask - A callback function to add a new task.
+ * @param onToggleTask - A callback function to toggle the status of a task.
+ * @param onDeleteTask - A callback function to delete a task.
+ * @param onEditTask - A callback function to edit a task.
+ * @returns The task template component.
+ */
+export const TaskTemplate = (tasks: Task[], onAddTask: (name: string) => void, onToggleTask: (id: string) => void, onDeleteTask: (id: string) => void, onEditTask: (id: string, newName: string) => void) => {
+    const container = document.createElement('div');
+
+    const form = document.createElement('div');
+    form.setAttribute('role', 'form');
+    form.setAttribute('aria-labelledby', 'add-task-form');
+
+    const formLabel = document.createElement('label');
+    formLabel.id = 'add-task-form';
+    formLabel.setAttribute('for', 'new-task');
+    formLabel.innerText = 'Add a new task';
+
+    const input = Input('Add a new task', 'New task input');
+    input.id = 'new-task';
+
+    const addButton = Button('Add Task', () => {
+        if (input.value.trim()) {
+            onAddTask(input.value.trim());
+            input.value = '';
+        }
+    }, 'Add task');
+
+    form.appendChild(formLabel);
+    form.appendChild(input);
+    form.appendChild(addButton);
+
+    const taskList = TaskList(tasks, onToggleTask, onDeleteTask, onEditTask);
+
+    container.appendChild(form);
+    container.appendChild(taskList);
+
+    return container;
+};
